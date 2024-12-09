d30ce965049824b1694151d7f2116e13602ab356
mmokrzyc <01178234@pw.edu.pl>
2024-12-09 14:39:28 +0100

    Test are build but not passed

--- LLProxys/gpio/Gpio.c
+++ LLProxys/gpio/Gpio.c
@@ -11,34 +11,35 @@
 #include "Gpio.h"
 #include "main.h"
 
-void gpio_initialize(Gpio *gpio, GPIO_TypeDef *port, uint16_t pin)
-{
-    gpio->port = port;
-    gpio->pin = pin;
-}
-void gpio_configure(Gpio *gpio, GPIO_TypeDef *port, uint16_t pin)
+void gpio_initialize(Gpio *gpio, void *port, uint16_t pin)
 {
     gpio->port = port;
     gpio->pin = pin;
 }
 
-uint8_t gpio_access(Gpio *gpio)
+// Configure a GPIO pin
+void gpio_configure(Gpio *gpio, void *port, uint16_t pin)
 {
-    return HAL_GPIO_ReadPin(gpio->port, gpio->pin);
+    gpio->port = port;
+    gpio->pin = pin;
 }
 
-void gpio_set_pin(Gpio *gpio)
-{
-    HAL_GPIO_WritePin(gpio->port, gpio->pin, GPIO_PIN_SET);
-}
+// uint8_t gpio_access(Gpio *gpio)
+// {
+//     return HAL_GPIO_ReadPin(gpio->port, gpio->pin);
+// }
 
-void gpio_reset_pin(Gpio *gpio)
-{
-    HAL_GPIO_WritePin(gpio->port, gpio->pin, GPIO_PIN_RESET);
-}
+// void gpio_set_pin(Gpio *gpio)
+// {
+//     HAL_GPIO_WritePin(gpio->port, gpio->pin, GPIO_PIN_SET);
+// }
 
-void gpio_toggle_pin(Gpio *gpio)
-{
-    HAL_GPIO_TogglePin(gpio->port, gpio->pin);
-}
+// void gpio_reset_pin(Gpio *gpio)
+// {
+//     HAL_GPIO_WritePin(gpio->port, gpio->pin, GPIO_PIN_RESET);
+// }
 
+// void gpio_toggle_pin(Gpio *gpio)
+// {
+//     HAL_GPIO_TogglePin(gpio->port, gpio->pin);
+// }
--- LLProxys/gpio/Gpio.h
+++ LLProxys/gpio/Gpio.h
@@ -10,20 +10,21 @@
 
 #pragma once
 
+#include <stdint.h>
 // #include "stm_headers.h"
 //  #include HAL_GPIO
-#include "stm32l4xx.h"
-#include "stm32l4xx_hal_def.h"
+// #include "stm32l4xx.h"
+// #include "stm32l4xx_hal_def.h"
 
 typedef struct
 {
-    GPIO_TypeDef *port;
+    void *port;
     uint16_t pin;
 } Gpio;
 
-void gpio_initialize(Gpio *gpio, GPIO_TypeDef *port, uint16_t pin);
-void gpio_configure(Gpio *gpio, GPIO_TypeDef *port, uint16_t pin);
-uint8_t gpio_access(Gpio *gpio);
-void gpio_set_pin(Gpio *gpio);
-void gpio_reset_pin(Gpio *gpio);
-void gpio_toggle_pin(Gpio *gpio);
+void gpio_initialize(Gpio *gpio, void *port, uint16_t pin);
+void gpio_configure(Gpio *gpio, void *port, uint16_t pin);
+// uint8_t gpio_access(const Gpio *gpio);
+// void gpio_set_pin(const Gpio *gpio);
+// void gpio_reset_pin(const Gpio *gpio);
+// void gpio_toggle_pin(const Gpio *gpio);
