f665a941691eae22d7dd03140e8ab6a48d62d651
Michał Mokrzycki <01178234@pw.edu.pl>
2024-12-16 20:35:33 +0100

    Finish OLedProxy high level  imp

--- HLProxys/oled_display/OLedDisplayProxy.c
+++ HLProxys/oled_display/OLedDisplayProxy.c
@@ -2,19 +2,41 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+
 void OLEDProxy_initialize(BaseHLProxy *self)
 {
-
+    OLEDProxy *oledProxy = (OLEDProxy *)self; 
+    ssd1306_Init();  
+    oledProxy->clear(oledProxy);  
+    oledProxy->update_display(oledProxy); 
 }
 
 void OLEDProxy_execute(BaseHLProxy *self, const char *action)
 {
-    
+    OLEDProxy *oledProxy = (OLEDProxy *)self;
+    // actions to define later
 }
 
 void OLEDProxy_shutdown(BaseHLProxy *self)
 {
+    OLEDProxy *oledProxy = (OLEDProxy *)self; 
+    ssd1306_Fill(Black);  
+    oledProxy->update_display(oledProxy);
 
+    free(oledProxy);
+}
+
+
+void OLEDProxy_clear()
+{
+    ssd1306_Fill(Black);
+    ssd1306_UpdateScreen();
+}
+
+void OLEDProxy_update_display()
+{
+    ssd1306_UpdateScreen();
 }
 
 
@@ -24,5 +46,6 @@ OLEDProxy *CreateOLEDProxy(const char *name) {
     proxy->base_proxy.initialize = OLEDProxy_initialize;
     proxy->base_proxy.execute = OLEDProxy_execute;
     proxy->base_proxy.shutdown = OLEDProxy_shutdown;
+    proxy->clear = OLEDProxy_clear;
     return proxy;
 }
--- HLProxys/oled_display/OLedDisplayProxy.h
+++ HLProxys/oled_display/OLedDisplayProxy.h
@@ -4,11 +4,14 @@
 #define OLEDDISPLAYPROXY_H
 
 #include "BaseHLProxy.h"
-#include "ssd1306.h"
+#include "oled_drivers.h"
 
 
 typedef struct OLEDProxy {
     BaseHLProxy base_proxy;    
+    // we can add more methods here, specific for OLED display
+    void (*clear)();
+    void (*update_display)();
 } OLEDProxy;
 
 OLEDProxy *CreateOLEDProxy(const char *name);
