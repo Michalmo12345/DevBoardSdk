64354e21394eb429eadfbad257d30bc1daad60f2
mmokrzyc <01178234@pw.edu.pl>
2024-12-28 23:46:46 +0100

    Define register addresses and start to impl read and write functions

--- HLProxys/rf_module/RFModuleProxy.c
+++ HLProxys/rf_module/RFModuleProxy.c
@@ -1,10 +1,30 @@
 #include "RFModuleProxy.h"
 
-void RFModuleProxy_initialize(BaseHLProxy *self) {}
+void RFModuleProxy_initialize(BaseHLProxy *self)
+{
+    RFModuleProxy *rf_module_proxy = (RFModuleProxy *)self;
+}
 
-void RFModuleProxy_execute(BaseHLProxy *self, const char *action) {}
+void RFModuleProxy_execute(BaseHLProxy *self, const char *action)
+{
+    RFModuleProxy *rf_module_proxy = (RFModuleProxy *)self;
+    // actions to define later
+}
 
-void RFModuleProxy_shutdown(BaseHLProxy *self) {}
+void RFModuleProxy_shutdown(BaseHLProxy *self)
+{
+    RFModuleProxy *rf_module_proxy = (RFModuleProxy *)self;
+}
+
+void RFModuleProxy_read(uint8_t reg)
+{
+    // read from RF module
+}
+
+void RFModuleProxy_write(uint8_t reg, uint8_t data)
+{
+    // write to RF module
+}
 
 RFModuleProxy CreateRFModuleProxy(const char *name)
 {
--- HLProxys/rf_module/RFModuleProxy.h
+++ HLProxys/rf_module/RFModuleProxy.h
@@ -3,10 +3,20 @@
 #ifndef RFMODULEPROXY_H
 #define RFMODULEPROXY_H
 
+#define SX1276_REG_FIFO 0x00
+#define SX1276_REG_OP_MODE 0x01
+#define SX1276_REG_BITRATE_MSB 0x02
+#define SX1276_REG_BITRATE_LSB 0x03
+#define SX1276_REG_FDEV_MSB 0x04
+#define SX1276_REG_FDEV_LSB 0x05
+#define SX1276_REG_FRF_MSB 0x06
+
 #include "BaseHLProxy.h"
 
 typedef struct RFModuleProxy {
     BaseHLProxy base_proxy;
+    void (*read)(uint8_t reg);
+    void (*write)(uint8_t reg, uint8_t data);
 } RFModuleProxy;
 
 RFModuleProxy CreateRFModuleProxy(const char *name);
