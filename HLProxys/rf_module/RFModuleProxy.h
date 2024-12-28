3b738fc171595f858052f8547214831de41ebce9
mmokrzyc <01178234@pw.edu.pl>
2024-12-28 21:58:16 +0100

    Add basic rfmoduleproxy

---
+++ HLProxys/rf_module/RFModuleProxy.h
@@ -0,0 +1,14 @@
+
+
+#ifndef RFMODULEPROXY_H
+#define RFMODULEPROXY_H
+
+#include "BaseHLProxy.h"
+
+typedef struct RFModuleProxy {
+    BaseHLProxy base_proxy;
+} RFModuleProxy;
+
+RFModuleProxy *CreateRFModuleProxy(const char *name);
+
+#endif // RFMODULEPROXY_H
